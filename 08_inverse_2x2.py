c6e4ca67a1895af628980a97875802c50db24e14
MohithCiga <39794006+MohithCiga@users.noreply.github.com>
2025-03-19 23:08:48 +0530

    Create 08_inverse_2x2.py

---
+++ 08_inverse_2x2.py
@@ -0,0 +1,10 @@
+def inverse_2x2(matrix: list[list[float]]) -> list[list[float]]:
+
+	determinant = matrix[0][0]*matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+	inverse = ([ [matrix[1][1], -1*matrix[0][1] ],
+				     [-1*matrix[1][0], matrix[0][0]]  ]) 
+				
+	inverse = [[i/ (determinant) for i in inverse[0]],
+				     [i/ (determinant) for i in inverse[1]]]
+	return inverse
